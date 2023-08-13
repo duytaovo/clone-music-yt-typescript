3707f6b2a217d019e9c1349d753a8a8ce74d1093
duytaovo <duytaovo@gmail.com>
2023-08-13 16:43:40 +0700

    update

---
+++ src/hooks/useRoutesHistory.tsx
@@ -0,0 +1,48 @@
+import { useEffect, useRef, useState } from "react"
+import { useAppDispatch } from "./useRedux"
+import { useLocation, useNavigate, useSearchParams } from "react-router-dom"
+import { setRoute } from "src/store/slices/route"
+import { IPath} from "src/types/types.type"
+import pathConfig from 'src/constants/path'
+// import { playSong, showPlayerInfo } from "src/utils/utils"
+
+
+const useRouteHistory = () => {
+  const dispatch = useAppDispatch()
+  const prePath = useRef<IPath>({})
+  const midlePath = useRef<IPath>({})
+  const path: IPath = useLocation()
+  const navigate = useNavigate()
+  const [searchParams, setSearchParams] = useSearchParams()
+  // const playListId = useAppSelector((state) => state.player?.playListId)
+
+
+  useEffect(() => {
+    midlePath.current = path
+    if (path?.pathname !== pathConfig.player) {
+      dispatch(
+        setRoute({
+          prePath: prePath.current,
+          currentPath: path,
+        }),
+      )
+      // showPlayerInfo(false)
+    } else {
+      const id = searchParams.get("id") || ""
+      const _idPlayList = searchParams.get("idPlayList") || ""
+      // playSong({ id, idPlayList: _idPlayList || "000" })
+    }
+  }, [path])
+
+  if (midlePath.current?.pathname !== path?.pathname) {
+    prePath.current = midlePath.current
+  }
+
+  return {
+    prePath: prePath.current,
+    currentPath: path,
+  }
+}
+  
+
+export default useRouteHistory
--- src/store/slices/playlist/index.ts
+++ src/store/slices/playlist/index.ts
@@ -1,31 +1,32 @@
 // src/features/userSlice.ts
 
-import { createAsyncThunk, createSlice, PayloadAction } from '@reduxjs/toolkit';
-import playListApi from 'src/apis/playList.api';
-import { payloadCreator } from 'src/utils/utils';
-
+import { createAsyncThunk, createSlice, PayloadAction } from '@reduxjs/toolkit'
+import playListApi from 'src/apis/playList.api'
+import { payloadCreator } from 'src/utils/utils'
 
 export const getPlayList = createAsyncThunk('playlist/getPlayList', payloadCreator(playListApi.getPlayList))
-export const getSongDetail = createAsyncThunk('playlist/getSongDetail', payloadCreator(playListApi.getDetailSongFromPlayList))
+export const getSongDetail = createAsyncThunk(
+  'playlist/getSongDetail',
+  payloadCreator(playListApi.getDetailSongFromPlayList)
+)
 export const getSongSound = createAsyncThunk('playlist/getSongSound', payloadCreator(playListApi.getSongSound))
 
 interface SongState {
-  playlist: any,
-  idPlayList:string,
-  routes:string[],
-  routePre:string,
-  routeCurrent:string,
-  ref:any,
+  playlist: any
+  idPlayList: string
+  routes: string[]
+  routePre: string
+  routeCurrent: string
+  ref: any
 }
 
 const initialState: SongState = {
   playlist: [],
-  idPlayList:"",
-  routes:["/",""],
-  routePre:"/",
-  routeCurrent:"",
-  ref:"",
-
+  idPlayList: '',
+  routes: ['/', ''],
+  routePre: '/',
+  routeCurrent: '',
+  ref: ''
 }
 
 const playListSlice = createSlice({
@@ -33,37 +34,38 @@ const playListSlice = createSlice({
   initialState,
   reducers: {
     addUrlToHistory: (state, action: PayloadAction<string>) => {
-      state.routes.push(action.payload);
+      state.routes.push(action.payload)
     },
+
     removeFirstUrl: (state) => {
-      state.routes.shift();
+      state.routes.shift()
     },
     updateHistory: (state, action: PayloadAction<string>) => {
-      const newUrl = action.payload;
-      const currentUrl = state.routes[1];
+      const newUrl = action.payload
+      const currentUrl = state.routes[1]
       if (newUrl !== currentUrl) {
         // Nếu route mới khác với route hiện tại, thực hiện cập nhật lịch sử
-        state.routes = state.routes.slice(2); // Xóa route đầu tiên
-        state.routes.push(newUrl); // Đẩy route mới vào đầu mảng
+        state.routes = state.routes.slice(2) // Xóa route đầu tiên
+        state.routes.push(newUrl) // Đẩy route mới vào đầu mảng
       }
     },
-    updateRoutePre:(state,action:PayloadAction<string>) =>{
+    updateRoutePre: (state, action: PayloadAction<string>) => {
       state.routePre = action.payload
-      localStorage.setItem("routePre", action.payload)
-
+      localStorage.setItem('routePre', action.payload)
     },
-    updateIdPlayList:(state,action:PayloadAction<string>) =>{
+    updateIdPlayList: (state, action: PayloadAction<string>) => {
       state.idPlayList = action.payload
-    },
+    }
   },
-  extraReducers: builder => {
+  extraReducers: (builder) => {
     // The `builder` callback form is used here because it provides correctly typed reducers from the action creators
     builder.addCase(getPlayList.fulfilled, (state, { payload }) => {
       state.playlist = payload
     })
   }
-});
+})
 
-export const {addUrlToHistory,removeFirstUrl,updateHistory,updateRoutePre,updateIdPlayList} = playListSlice.actions
-const playListReducer = playListSlice.reducer;
+export const { addUrlToHistory, removeFirstUrl, updateHistory, updateRoutePre, updateIdPlayList } =
+  playListSlice.actions
+const playListReducer = playListSlice.reducer
 export default playListReducer
